7b1beb16954586ded5c6ef04bde778faf70afe89
Djanis00 <162571774+Djanis00@users.noreply.github.com>
2025-06-06 16:18:15 +0200

    test: année divisible par 400 est bissextile

--- bissextile.test/UnitTest1.cs
+++ bissextile.test/UnitTest1.cs
@@ -4,9 +4,24 @@ using bissextile;
 public class BissextileTests
 {
     [Fact]
-    public void Annee_divisible_par_400_est_bissextile()
+    public void Annee_1600_est_bissextile()
+    {
+        var result = DateUtils.IsBissextile(1600);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Annee_2000_est_bissextile()
     {
         var result = DateUtils.IsBissextile(2000);
         Assert.True(result);
     }
+
+    [Fact]
+    public void Annee_2400_est_bissextile()
+    {
+        var result = DateUtils.IsBissextile(2400);
+        Assert.True(result);
+    }
+
 }
